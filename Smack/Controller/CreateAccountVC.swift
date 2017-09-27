73a48bdb26b0982674adb6c722cf91a9f208e6a6
Steffen Jordan Meilsoe <Steffen@Steffens-MBP.gateway>
2017-09-27 15:25:14 +1000

    commit before restarting

---
+++ Smack/Controller/CreateAccountVC.swift
@@ -0,0 +1,19 @@
+//
+//  CreateAccountVC.swift
+//  Smack
+//
+//  Created by Steffen Jordan Meilsoe on 9/27/17.
+//  Copyright © 2017 Lucrement IVS. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
