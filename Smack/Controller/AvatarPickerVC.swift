1873a09e16b3b9688d9e489077b823d6e3f40ee6
Steffen Jordan Meilsoe <Steffen@Steffens-MacBook-Pro.local>
2017-09-30 18:51:07 +1000

    Lesson-13

---
+++ Smack/Controller/AvatarPickerVC.swift
@@ -0,0 +1,52 @@
+//
+//  AvatarPickerVC.swift
+//  Smack
+//
+//  Created by Steffen Jordan Meilsoe on 9/30/17.
+//  Copyright © 2017 Lucrement IVS. All rights reserved.
+//
+
+import UIKit
+
+class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    //Outlets
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AvatarCell", for: indexPath) as? AvatarCell {
+            return cell
+        }
+        
+        return AvatarCell()
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 28
+    }
+
+    @IBAction func backPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func segmentControlChanged(_ sender: Any) {
+    }
+    
+    
+    
+    
+
+}
