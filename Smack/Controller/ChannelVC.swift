93b358b56bb289079d53aa4f644ef651d451d0e7
Steffen Jordan Meilsoe <Steffen@MACBOOKPRO-5709.gateway>
2017-10-15 21:24:50 +1100

    finished avatar color and choice to follow to channelVC

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -13,6 +13,7 @@ class ChannelVC: UIViewController {
     //Outlets
     @IBOutlet weak var loginButton: UIButton!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
+    @IBOutlet weak var userImage: CircleImage!
     
     
     override func viewDidLoad() {
@@ -20,6 +21,7 @@ class ChannelVC: UIViewController {
         
         //STM: Remove this addition for future projects, where you want to see more of the underlying slide
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIFICATION_USER_DATA_DID_CHANGE, object: nil)
     }
     
     //IBActions
@@ -27,6 +29,16 @@ class ChannelVC: UIViewController {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
     
-    
+    @objc func userDataDidChange(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            loginButton.setTitle(UserDataService.instance.name, for: .normal)
+            userImage.image = UIImage(named: UserDataService.instance.avatarName)
+            userImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            loginButton.setTitle("Login", for: .normal)
+            userImage.image = UIImage(named: "menuProfileIcon")
+            userImage.backgroundColor = UIColor.clear
+        }
+    }
     
 }
